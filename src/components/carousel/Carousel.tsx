7182527fc6b3dcf41d9c9c85c506d41256a36f3a
PamanAleph <alieffirmandabisnis@gmail.com>
2024-08-22 20:21:25 +0700

    fix styling

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -28,6 +28,10 @@ const Carousel = () => {
 
   return (
     <>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+      />
       <Script
         src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
         strategy="beforeInteractive"
